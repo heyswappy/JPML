45984535726b1cde093c4cb844b6312a8b617a2f
Swapnil Saxena <smart.swapnil.saxena@gmail.com>
2022-02-04 22:25:16 +0530

    Modified driver code to account for Style as a class.

--- src/Exec/Sample/SampleTemplate.java
+++ src/Exec/Sample/SampleTemplate.java
@@ -2,9 +2,9 @@ package Exec.Sample;
 
 import Document.ConcreteWebDocument;
 import Document.WebDocument;
+import Style.Style;
+import Style.ConcreteStyle;
 import Template.AbstractDocumentTemplate;
-
-import static Constants.JPMLConstants.NO_ATTRIBUTES;
 
 public class SampleTemplate extends AbstractDocumentTemplate<SampleData> {
 
@@ -12,47 +12,50 @@ public class SampleTemplate extends AbstractDocumentTemplate<SampleData> {
     public WebDocument build(WebDocument document, SampleData data) throws Exception {
         // implement logic
         SampleDataPresenter presenter = data.presenter;
-        document.appendTag("html", NO_ATTRIBUTES);
+        document.appendTag("html");
         {
-            document.appendTag("head", NO_ATTRIBUTES);
+            document.appendTag("head");
             document.closeLatestTag();
-            document.appendTag("body", NO_ATTRIBUTES);
+            document.appendTag("body");
             for(int i=0; i<10; i++) {
-                document.appendTag("b", NO_ATTRIBUTES);
+                document.appendTag("b");
                 document.insertData(String.valueOf(i));
                 document.closeLatestTag();
-                document.appendTag("br", NO_ATTRIBUTES);
+                document.appendTag("br");
             }
             //
-            document.appendTag("b", NO_ATTRIBUTES);
+            document.appendTag("b");
             document.insertData(presenter.s);
             document.closeLatestTag();
-            document.appendTag("br", NO_ATTRIBUTES);
+            document.appendTag("br");
             //
-            document.appendTag("b", NO_ATTRIBUTES);
+            document.appendTag("b");
             document.insertData(presenter.d.toString());
             document.closeLatestTag();
-            document.appendTag("br", NO_ATTRIBUTES);
+            document.appendTag("br");
             //
-            document.appendTag("b", NO_ATTRIBUTES);
+            document.appendTag("b");
             document.insertData(presenter.i.toString());
             document.closeLatestTag();
-            document.appendTag("br", NO_ATTRIBUTES);
+            document.appendTag("br");
             //
-            document.appendTag("b", NO_ATTRIBUTES);
+            document.appendTag("b");
             document.insertData(presenter.b.toString());
             document.closeLatestTag();
-            document.appendTag("br", NO_ATTRIBUTES);
+            document.appendTag("br");
             //
-            document.appendTag("b", NO_ATTRIBUTES);
+            document.appendTag("b");
             document.insertData(presenter.f.toString());
             document.closeLatestTag();
-            document.appendTag("br", NO_ATTRIBUTES);
+            document.appendTag("br");
             //
-            document.appendTag("b", NO_ATTRIBUTES);
+            Style t = new ConcreteStyle();
+            t.addStyle("color", "blue");
+            t.addStyle("border", "1px dashed red");
+            document.appendTag("b", t);
             document.insertData(presenter.l.toString());
             document.closeLatestTag();
-            document.appendTag("br", NO_ATTRIBUTES);
+            document.appendTag("br");
             //
             document.closeLatestTag();
         }
